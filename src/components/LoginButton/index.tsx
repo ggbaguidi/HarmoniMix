c5cc6ee104a63317bc50a0ad1a4c2f61da2e89be
Guy Ahonakpon GBAGUIDI <guygbaguidi123root@gmail.com>
2023-12-14 23:12:38 +0100

    ci: modify the uri

--- src/components/LoginButton/index.tsx
+++ src/components/LoginButton/index.tsx
@@ -4,7 +4,7 @@ import './style.css';
 
 
 const LoginButton = () => {
-    const MY_WEBSITE: string = "https://harmonimix.netlify.app/callback";
+    const MY_WEBSITE: string = "https://harmonimix.vercel.app/callback";
     const REDIRECT_URL: string = `https://accounts.spotify.com/authorize?client_id=${process.env.REACT_APP_CLIENT_ID}&response_type=token&redirect_uri=${MY_WEBSITE}&scope=playlist-modify-private`
 
     return (
