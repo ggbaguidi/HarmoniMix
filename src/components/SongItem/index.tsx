d3c62223342725c9b77bdea03fb0c41c72051a46
Guy Ahonakpon GBAGUIDI <guygbaguidi123root@gmail.com>
2023-12-20 17:10:39 +0100

    docs: add comment to SongItem component

--- src/components/SongItem/index.tsx
+++ src/components/SongItem/index.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import './style.css';
 import PropTypes from 'prop-types';
 
+// Define the Props type for the component
 type Props = {
     type: string,
     status: boolean,
@@ -15,54 +16,58 @@ type Props = {
     artist: string
 }
 
+// SongItem component function
 const SongItem = ({
-    type,status,pushToSelectedList,deleteFromSelectedList,id,imgUrl,songTitle,artistLink,artist
+    type, status, pushToSelectedList, deleteFromSelectedList, id, imgUrl, songTitle, artistLink, artist
 }: Props) => {
+    // State hook to manage the status of the item
+    const [statusState, setStatusState] = useState(status)
 
-    const [statusState,setStatusState] = useState(status)
-
+    // Function to handle button click and update the status
     const showAlert = () => {
         setStatusState(!statusState)
-        if (!statusState){
+        // Call the appropriate function based on the status
+        if (!statusState) {
             pushToSelectedList(id);
         } else {
             deleteFromSelectedList(id);
         }
     }
 
+    // Conditional rendering for the button based on the status
     let button: any;
-    if (statusState === false){
+    if (statusState === false) {
         button = <button className="selectButton" onClick={showAlert}>Select</button>
     } else {
         button = <button className="deselectButton" onClick={showAlert}>Deselect</button>
     }
 
-    console.log(type)
-    if (type=="recommendation"){
+    // Render the SongItem component based on the type
+    if (type === "recommendation") {
         return (
             <div className="itemContainer">
-                <img src={imgUrl} alt={`${songTitle}`}/>
+                <img src={imgUrl} alt={`${songTitle}`} />
                 <p className="songTitle">{songTitle}</p>
                 <a className="artist" href={artistLink}>{artist}</a>
             </div>
         )
     }
 
+    // Render the SongItem component with the button
     return (
-            <div className="itemContainer">
-                <img src={imgUrl} alt={`${songTitle}`}/>
-                <p className="songTitle">{songTitle}</p>
-                <a className="artist" href={artistLink}>{artist}</a>
-                <br></br>
-                {button}
-            </div>
+        <div className="itemContainer">
+            <img src={imgUrl} alt={`${songTitle}`} />
+            <p className="songTitle">{songTitle}</p>
+            <a className="artist" href={artistLink}>{artist}</a>
+            <br></br>
+            {button}
+        </div>
     );
 }
 
-export default SongItem;
-
+// Prop type validation for SongItem component
 SongItem.propTypes = {
-    type: PropTypes.bool,
+    type: PropTypes.string,
     status: PropTypes.bool,
     pushToSelectedList: PropTypes.any,
     id: PropTypes.string,
@@ -72,3 +77,6 @@ SongItem.propTypes = {
     artistLink: PropTypes.string,
     artist: PropTypes.string,
 }
+
+// Export the SongItem component for use in other files
+export default SongItem;
