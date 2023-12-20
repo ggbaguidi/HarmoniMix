137d33e255a1bcb2df32b804e6f7af6e81c947d7
Guy Ahonakpon GBAGUIDI <guygbaguidi123root@gmail.com>
2023-12-20 17:11:28 +0100

    docs: add comment to TopNavBar component

--- src/components/TopNavBar/index.jsx
+++ src/components/TopNavBar/index.jsx
@@ -1,32 +1,38 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './style.css'
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
+// TopNavBar component function
 const TopNavBar = (props) => {
-
-    if (props.title == "home"){
+    // Conditional rendering based on the 'title' prop
+    if (props.title === "home") {
         return (
             <div className="containerTopNavBar">
                 <h2 className="selected">Create Playlists</h2>
+                {/* Link to the recommendations page */}
                 <Link to='/recommendations'>
-                    <h2 className="unselected">Reccomendations</h2>
+                    <h2 className="unselected">Recommendations</h2>
                 </Link>
             </div>
         )
     }
+    // Render when 'title' is not "home"
     return (
         <div className="containerTopNavBar">
+            {/* Link to the create-playlist page */}
             <Link to='create-playlist'>
                 <h2 className="unselected">Create Playlists</h2>
             </Link>
-            <h2 className="selected">Reccomendations</h2>
+            <h2 className="selected">Recommendations</h2>
         </div>
     )
 }
 
-export default TopNavBar;
-
+// Prop type validation for TopNavBar component
 TopNavBar.propTypes = {
-    title : PropTypes.string
+    title: PropTypes.string
 }
+
+// Export the TopNavBar component for use in other files
+export default TopNavBar;
