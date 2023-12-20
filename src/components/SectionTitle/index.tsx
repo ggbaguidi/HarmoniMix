f0e54ae3170bcbdd2e8e0a56094211602928c23a
Guy Ahonakpon GBAGUIDI <guygbaguidi123root@gmail.com>
2023-12-20 17:09:03 +0100

    docs: add comment to SectionTitle component

--- src/components/SectionTitle/index.tsx
+++ src/components/SectionTitle/index.tsx
@@ -2,18 +2,23 @@ import React from 'react';
 import './style.css';
 import PropTypes from 'prop-types';
 
+// Define the Props type for the component
 type Props = {
     title: string
 }
 
-const SectionTitle = ({title}: Props) => {
+// SectionTitle component function
+const SectionTitle = ({ title }: Props) => {
+    // Render an h1 element with the specified title and a CSS class
     return (
         <h1 className="songsTitle">{title}</h1>
     );
 }
 
-export default SectionTitle;
-
+// Prop type validation for SectionTitle component
 SectionTitle.propTypes = {
     title: PropTypes.string
 }
+
+// Export the SectionTitle component for use in other files
+export default SectionTitle;
